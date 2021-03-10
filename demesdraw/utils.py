5c708dedcb7aa3ec87a09fb63eda89d215e640c9
Graham Gower <graham.gower@gmail.com>
2021-03-10 16:28:54 +0100

    Fix time scale when a deme branches off the root.

--- demesdraw/utils.py
+++ demesdraw/utils.py
@@ -11,8 +11,13 @@ def inf_start_time(graph: demes.Graph, inf_ratio: float, log_scale: bool) -> flo
     :return: The time
     :rtype: float
     """
-    oldest_end_time = max(deme.epochs[0].end_time for deme in graph.demes)
-    if oldest_end_time == 0:
+    epoch0_times = []
+    for deme in graph.demes:
+        epoch0_times.append(deme.epochs[0].end_time)
+        if not np.isinf(deme.epochs[0].start_time):
+            epoch0_times.append(deme.epochs[0].start_time)
+    oldest_noninf_time = max(epoch0_times)
+    if oldest_noninf_time == 0:
         # All demes are root demes and have a constant size.
         # About 100 generations is a nice time scale to draw, and we use 113
         # specifically so that the infinity line extends slightly beyond the
@@ -20,7 +25,7 @@ def inf_start_time(graph: demes.Graph, inf_ratio: float, log_scale: bool) -> flo
         inf_start_time: float = 113
     else:
         if log_scale:
-            inf_start_time = np.exp(np.log(oldest_end_time) / (1 - inf_ratio))
+            inf_start_time = np.exp(np.log(oldest_noninf_time) / (1 - inf_ratio))
         else:
-            inf_start_time = oldest_end_time / (1 - inf_ratio)
+            inf_start_time = oldest_noninf_time / (1 - inf_ratio)
     return inf_start_time
